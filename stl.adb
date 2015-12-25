1929a6b9519eac4e8e89ffd1d32acf772e6c3bf4
rvdz <robin.vdeleuze@gmail.com>
2015-12-25 16:51:19 +0100

    stl brainstorming

--- stl.adb
+++ stl.adb
@@ -7,10 +7,34 @@ use Ada.Numerics.Elementary_Functions;
 
 package body STL is
 
-    procedure Creation(Segments : in out Liste_Points.Liste ;
-        Facettes :    out Liste_Facettes.Liste) is
+    function Decaler(P : Point2D; Angle_Radian : Float) return Point3D is
     begin
-        null;
+        return (1 => P(P'First) * Cos(Angle_Radian),
+                2 => P(P'Last),
+                3 => P(P'First) * Sin(Angle_Radian));
+    end;
+
+    procedure Creation(Segments : in out Liste_Points.Liste ;
+        Facettes : out Liste_Facettes.Liste) is
+        Premier_Point : Point3D;
+    begin
+        -- L'idée est de parcourir l'axe y=0 pour CHAQUE angle
+        -- et de créer des morceaux de "disques" à chaque fois
+        -- C'est donc le pas qui varie en premier
+        -- (plus facile de cette manière pour créer des triplets de points)
+        for Pas in 1..M loop
+            -- On crée d'abord le premier point décalé d'un angle 2*PI/Pas radian
+            --Premier_Point := Decaler(Tete(Segments), (2.0 * PI)/Float(Pas));
+            for I in 2..Liste_Points.Taille(Segments) loop
+                null;
+                -- TODO Créer une procedure de type Parcourir
+                -- qui va créer deux facettes à chaque point créé.
+                -- TODO Création des points sur le "disque"
+                -- avec de la trigonométrie du swag.
+                -- On crée un point, on crée 2 facettes !
+                -- On insère deux Facettes (i.e. un groupe de 3 points) à chaque fois
+            end loop;
+        end loop;
     end;
 
     procedure Sauvegarder(Nom_Fichier : String ;
--- stl.ads
+++ stl.ads
@@ -1,18 +1,30 @@
+-- TODO Prétraitement : décalage de xmin et ymin
 with Liste_Generique;
 with Math; use Math;
 
 package STL is
+    -- Pas de la rotation
+    M : constant Positive := 30;
+
+    subtype Point3D is Vecteur(1..3);
+
     type Facette is record
-        P1, P2, P3 : Vecteur(1..3);
+        P1, P2, P3 : Point3D;
     end record;
 
     package Liste_Facettes is new Liste_Generique(Facette);
 
     --prend une liste de segments et cree l'objet 3d par rotations
+    -- Requiert Taille(Segments) > 0
     procedure Creation(Segments : in out Liste_Points.Liste ;
-        Facettes :    out Liste_Facettes.Liste);
+        Facettes : out Liste_Facettes.Liste);
 
     --sauvegarde le fichier stl
     procedure Sauvegarder(Nom_Fichier : String ;
         Facettes : Liste_Facettes.Liste);
+
+private
+    -- Crée un point 3D à partir d'un point 2D et d'un angle de rotation
+    -- Requiert un angle en radian
+    function Decaler(P : Point2D; Angle_Radian : Float) return Point3D;
 end;
