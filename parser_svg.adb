7a71b665dbff3783ca2267a8c59d5b9efe8aaabf
rvdz <robin.vdeleuze@gmail.com>
2015-12-26 15:38:58 +0100

    Brainstorming parser

--- parser_svg.adb
+++ parser_svg.adb
@@ -3,9 +3,17 @@ use Ada.Text_IO, Ada.Float_Text_IO;
 
 package body Parser_Svg is
 
-   procedure Chargement_Bezier(Nom_Fichier : String; L : out Liste) is
-   begin
-      null;
-   end;
+    procedure Chargement_Bezier(Nom_Fichier : String; L : out Liste) is
+    begin
+        -- Première étape : on charge le fichier svg
+        -- Deuxième étape : on récupère la ligne commençant par "d="
+        -- Troisième étape : analyse de cette même ligne en gérant
+        -- les différents cas (mlhvcq et MLHVCQ) + enregistrement des segments
+        -- dans une liste chainée
+        -- Quatrième étape : conversion des points en courbe de Bézier
+        -- (enregistrement dans L)
+        -- Cinquième étape : Profit
+        null;
+    end;
 
 end;
