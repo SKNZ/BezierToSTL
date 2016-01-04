156bd5ac1d6edad7249ffd1b5888cf42604d52ab
sknz <florandara@gmail.com>
2016-01-04 10:16:00 +0100

    Refactorisation STL

--- stl.adb
+++ stl.adb
@@ -2,48 +2,52 @@ with Ada.Text_IO;
 use Ada.Text_IO;
 
 package body STL is
-    procedure Pre_Traitement(Segments : in out Liste_Points.Liste) is
-        Point_Tete : Point2D := Liste_Points.Tete(Segments);
-        Point_Queue : Point2D := Liste_Points.Queue(Segments);
-
-        -- Met à jour les minima X_Min et Y_Min
-        procedure Comparer_Min(P : in out Point2D) is
-        begin
-            -- On compare X_Min et l'abscisse du point P
-            if P(P'First) > X_Min then
-                X_Min := P(P'First);
-            end if;
-
-            -- On compare Y_Min et l'ordonnée du point P
-            if P(P'Last) > Y_Min then
-                Y_Min := P(P'Last);
-            end if;
-        end;
-
-        procedure Chercher_Min is new Liste_Points.Parcourir(Traiter => Comparer_Min);
-    begin
-        -- On instancie les minima
-        X_Min := Point_Tete(Point_Tete'First);
-        Y_Min := Point_Tete(Point_Tete'Last);
-
-        -- On calcule les minima
-        Chercher_Min(Segments);
-
-        -- Si la coord. Y de début n'est pas sur 0.0, on
-        -- rajoute une coord. au début
-        if Point_Tete(Point_Tete'Last) /= 0.0 then
-            Liste_Points.Insertion_Tete(Segments, (Point_Tete(Point_Tete'First), 0.0));
-        end if;
-
-        -- Si la coord. Y de fin n'est pas sur 0.0, on
-        -- rajoute une coord. à la fin
-        if Point_Queue(Point_Queue'Last) /= 0.0 then
-            Liste_Points.Insertion_Queue(Segments, (Point_Queue(Point_Queue'First), 0.0));
-        end if;
-    end;
-
     procedure Creation(Segments : in out Liste_Points.Liste ;
         Facettes : out Liste_Facettes.Liste) is
+
+        -- Abscisses et ordonnées minimales nécessaires
+        -- pour le pré-traitement
+        X_Min, Y_Min : Float;
+
+        procedure Pre_Traitement(Segments : in out Liste_Points.Liste) is
+            Point_Tete : Point2D := Liste_Points.Tete(Segments);
+            Point_Queue : Point2D := Liste_Points.Queue(Segments);
+
+            -- Met à jour les minima X_Min et Y_Min
+            procedure Comparer_Min(P : in out Point2D) is
+            begin
+                -- On compare X_Min et l'abscisse du point P
+                if P(P'First) > X_Min then
+                    X_Min := P(P'First);
+                end if;
+
+                -- On compare Y_Min et l'ordonnée du point P
+                if P(P'Last) > Y_Min then
+                    Y_Min := P(P'Last);
+                end if;
+            end;
+
+            procedure Chercher_Min is new Liste_Points.Parcourir(Traiter => Comparer_Min);
+        begin
+            -- On instancie les minima
+            X_Min := Point_Tete(Point_Tete'First);
+            Y_Min := Point_Tete(Point_Tete'Last);
+
+            -- On calcule les minima
+            Chercher_Min(Segments);
+
+            -- Si la coord. Y de début n'est pas sur 0.0, on
+            -- rajoute une coord. au début
+            if Point_Tete(Point_Tete'Last) /= 0.0 then
+                Liste_Points.Insertion_Tete(Segments, (Point_Tete(Point_Tete'First), 0.0));
+            end if;
+
+            -- Si la coord. Y de fin n'est pas sur 0.0, on
+            -- rajoute une coord. à la fin
+            if Point_Queue(Point_Queue'Last) /= 0.0 then
+                Liste_Points.Insertion_Queue(Segments, (Point_Queue(Point_Queue'First), 0.0));
+            end if;
+        end;
 
         -- Crée un "cercle" de facettes
         procedure Creer_Facette(P_Cour, P_Suiv : in Point2D) is
